a453737032772240ff5e3a5e0cb17c8334570a07
yangchen <1165898906@qq.com>
2021-09-19 15:00:32 +0800

    第七次提交

--- src/main/java/testdemo.java
+++ src/main/java/testdemo.java
@@ -3,4 +3,8 @@ public class testdemo {
     public void te(int f,int o,double n){
         System.out.println("xinzen");
     }
+
+    public void ss(int f,int o,double n){
+        System.out.println("xinzen");
+    }
 }

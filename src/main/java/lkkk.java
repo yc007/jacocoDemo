ef9f16a0efdc1e189d46eb30516398ee568236f3
yangchen <1165898906@qq.com>
2021-09-23 11:22:33 +0800

    第十二次提交

--- src/main/java/lkkk.java
+++ src/main/java/lkkk.java
@@ -1,8 +1,16 @@
 public class lkkk {
 
-    public String kkk(){
+    public int kkk(){
 
+        int i= 10;
+        int j=12;
         
+        return i+j;
+    }
+
+
+    public String kkk11111(){
+
         return "ssdfsdf";
     }
 }
